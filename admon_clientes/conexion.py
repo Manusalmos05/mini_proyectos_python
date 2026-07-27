f25387f976442093863e208d16f7b45712b584a1
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-07-27 11:12:25 +0200

    Add Conexion class for MySQL connection pooling and management

---
+++ admon_clientes/conexion.py
@@ -0,0 +1,53 @@
+from mysql.connector import pooling
+from mysql.connector import Error
+
+
+class Conexion:
+    DATABASE='admon_clientes' 
+    USERNAME='root'
+    PASSWORD='Manuela0512*'
+    DB_PORT='3306'
+    HOST='localhost'
+    POOL_SIZE=5
+    POOL_NAME='admon_clientes_pool'
+    pool=None
+
+
+    @classmethod
+    def obtener_pool(cls):
+        if cls.pool is None: # se crea este ojbjeto pool
+            try:
+                cls.pool=pooling.MySQLConnectionPool(
+                    pool_name=cls.POOL_NAME,
+                    pool_size=cls.POOL_SIZE,
+                    host=cls.HOST,
+                    port=cls.DB_PORT,
+                    database=cls.DATABASE,
+                    user=cls.USERNAME,
+                    password=cls.PASSWORD
+                )
+                return cls.pool
+
+            except Error as e:
+                print(f'ocurrio un error al obtener pool: {e}')
+
+        else:
+            return cls.pool
+
+
+    @classmethod
+    def obtener_conexion(cls):
+        return cls.obtener_pool.get_connection()
+
+    @classmethod
+    def liberar_conexion(cls, conexion):
+        conexion.close()
+
+
+if __name__=='__main__':
+    pool=Conexion.obtener_pool()
+    print(pool)
+
+    conexion1=pool.get_connection()
+    print(conexion1)
+    Conexion.liberar_conexion(conexion1)
