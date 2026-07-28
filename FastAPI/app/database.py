4c69cceb42726f461d231b04db1858ec609c0b0f
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-07-28 18:49:57 +0200

    Add database setup and CRUD operations for Producto model in FastAPI

--- FastAPI/app/database.py
+++ FastAPI/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL="postgressql://postgres:Manuela0512*@localhost:5432/ecommerce_db"
+DATABASE_URL="postgresql://postgres:Manuela0512*@localhost:5432/ecommerce_db"
 
 
 engine=create_engine(DATABASE_URL)
