8328a6b273c42529f5efd6e29d4072ce09a6200a
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-08-04 20:50:41 +0200

    Refactor configuration loading; switch from os.getenv to Pydantic settings for DATABASE_URL

--- FastAPI/app/db/database.py
+++ FastAPI/app/db/database.py
@@ -2,13 +2,13 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base 
 from sqlalchemy.orm import sessionmaker
-import os
-load_dotenv()
+from core.security import settings
 
 
 
 
-DATABASE_URL=os.getenv("DATABASE_URL")
+
+DATABASE_URL=settings.DATABASE_URL
 
 
 engine=create_engine(DATABASE_URL)
