e32421627890a343dc28997ecb2de2ba8a1c1b62
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-08-07 10:28:56 +0200

    Add carrito CRUD functions and initial schema file; implement obtener_carrito and agregar_item functions

---
+++ FastAPI/app/crud/carrito.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from models.pedidos import Carrito, ItemCarrito
+
+
+def obtener_carrito(db: Session, usuario_id: int):
+    carrito= db.query(Carrito).filter(Carrito.usuario_id == usuario_id).first()
+    if carrito:
+        return carrito
+    else:
+        carrito= Carrito(usuario_id=usuario_id)
+        db.add(carrito)
+        db.commit()
+        db.refresh(carrito)
+        return carrito
+
+def agregar_item(
+        db:Session,
+        carrito_id:int,
+        producto_id:int,
+        cantidad:int = 1
+):
+    item = db.query(ItemCarrito).filter(carrito_id=carrito_id, producto_id=producto_id).first()
+    if item:
+        item.cantidad += cantidad
+    else:
+        item = ItemCarrito(carrito_id=carrito_id, producto_id=producto_id, cantidad=cantidad)
+        db.add(item)
+        db.commit()
+        db.refresh(item)
+        return item
