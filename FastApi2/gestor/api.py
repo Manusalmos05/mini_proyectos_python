535a6ea61015a64c524af4c4e74d0b9be4eb90c9
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-08-18 17:45:39 +0200

    Enhance client management API with validation and CRUD operations; update client model and add new client data

--- FastApi2/gestor/api.py
+++ FastApi2/gestor/api.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+import database as db
+from pydantic import BaseModel, constr, field_validator
+import helpers
+
+class ModeloCliente(BaseModel):
+    dni: str=constr(min_length=3, max_length=3)
+    nombre: str=constr(min_length=2, max_length=30)
+    apellido:str=constr(min_length=2, max_length=30)
+
+
+class ModeloCrearCliente(ModeloCliente):
+    @field_validator("dni")
+    def validar_dni(cls, dni):
+        if helpers.dni_valido(dni, db.Clientes.lista):
+            return dni
+        raise ValueError("Ya existe este cliente o el DNI es incorrecto")
+
+
+headers={"content-type":"charset=utf-8"}
 
 app = FastAPI()
 @app.get("/")
@@ -8,6 +26,46 @@ async def index():
     content={
         "mensaje": "mi segunda api"
     }
-    return JSONResponse(content=content)
+    return JSONResponse(content=content, headers=headers, media_type="aplication/json")
+
+@app.get("/html/")
+async def html():
+    content="""
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>¡Hola mundo!</title>
+    </head>
+    <body>
+        <h1>¡Hola mundo!</h1>
+    </body>
+    </html>
+    """
+    return Response(content=content,media_type="text/html")
+
+@app.get("/clientes")
+async def clientes():
+    content=[cliente.to_dict() for cliente in db.Clientes.lista]
+    return JSONResponse(content=content, headers=headers)
+
+@app.get("/clientes/buscar/{dni}")
+async def clientes_buscar(dni:str):
+    cliente=db.Clientes.buscar(dni=dni)
+    if cliente is None:
+        raise HTTPException(status_code=404, detail="No existe este cliente")
+    return JSONResponse(content=cliente.to_dict(), headers=headers)
+
+@app.post("/clientes/crear/")
+async def clientes_crear(datos: ModeloCrearCliente):
+    cliente=db.Clientes.crear(datos.dni, datos.nombre, datos.apellido)
+    if cliente:
+        return JSONResponse(content=cliente.to_dict(), headers=headers)
+    raise HTTPException(status_code=401, details="cliente no creado")
+
+
+
+
+
 
 print("Servidor de la api")
--- FastApi2/gestor/database.py
+++ FastApi2/gestor/database.py
@@ -11,6 +11,11 @@ class Cliente:
     def __str__(self):
         return f"({self.dni}) {self.nombre} {self.apellido}"
 
+    def to_dict(self):
+        return {"dni": self.dni,
+              "nombre": self.nombre,
+              "apellido": self.apellido}
+
 
 class Clientes:
 
