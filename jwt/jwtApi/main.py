daf44d6947cff936bf07ba80ea7c3372f7c30860
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-09-17 13:23:04 +0200

    inclui un middleware para calcular tiempo de llamadas

--- jwt/jwtApi/main.py
+++ jwt/jwtApi/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from config.db import engine, Base
 from routers import auth,users
-
+import time
 app= FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -9,5 +9,15 @@ Base.metadata.create_all(bind=engine)
 app.include_router(users.router, prefix="/users", tags=['users'])
 app.include_router(auth.router, prefix="/auth",tags=['auth'])
 
+
+@app.middleware("http")
+async def timing_middleware(request: Request, call_next):
+
+    start_time=time.time()
+    response=await call_next(request)
+    process_time=time.time()-start_time
+    response.headers["X-Process-Time"]=f"{process_time:.4f}s"
+    print(f"{request.method} {request.url.path} -> {response.status_code}: {process_time: .4f}s")
+    return response
 
 
