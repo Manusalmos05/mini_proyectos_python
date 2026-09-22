fce0cf815def92c7764ce9e3dd9fdfd9a2f7d608
Manuelasalmos05 <manuelasalazarmoscoso@gmail.com>
2026-09-22 14:42:04 +0200

    Add Selenium script for automated Google search and update test files

---
+++ selenium/pruebas_pytest/main.py
@@ -0,0 +1,10 @@
+def test_prueba():
+    assert True
+
+
+def test_pueba2():
+    assert True
+
+
+def test_prueba3():
+    assert True==False
---
+++ selenium/pruebas_pytest/test_main.py
@@ -0,0 +1,12 @@
+def test_prueba():
+    assert True
+
+
+def test_pueba2():
+    assert True
+
+
+
+#fallo
+def test_prueba3():
+    assert True==False
